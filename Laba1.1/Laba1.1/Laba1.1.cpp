e0d2f67009c608255d149a7e415fd3870a7825bc
Evdacheva <55709000+Evdacheva@users.noreply.github.com>
2020-09-30 09:39:14 +0300

    Add files via upload

---
+++ Laba1.1/Laba1.1/Laba1.1.cpp
@@ -0,0 +1,63 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+//Функция
+void print(float arr[], int n)
+{
+	printf("\n\nArray:\n");
+	for (int i = 0; i < n; i++)
+	{
+		printf("| %f ", arr[i]);
+	}
+}
+
+float processArray(float arr[], int n, float a, float b)
+{
+	srand((unsigned int)time(NULL));
+	int pos = 0;
+	//рандомные числса
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = ((b - a) * ((float)rand() / RAND_MAX)) + a;
+	}
+	print(arr, n);
+	//ищем максимальный эл.
+	float max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+			pos = i;
+		}
+	}
+	
+	for (int i = 0; i < n; i++)
+		arr[i] += max;
+
+	float* newArr = new float[n];
+	//новый массив
+	for (int i = 0; i < n; i++)
+	{
+		if (i > pos)
+			newArr[i] = arr[i];
+		else
+			newArr[i] = max;
+	}
+	print(newArr, n);
+	return max;
+	delete[] newArr;
+
+}
+
+void main()
+{
+	const int n = 20;
+	float a = -20, b = 70;
+	float arr[n] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	printf("\n\nMax num of array is %f\n", processArray(arr, n, a, b));
+}
+
+    
---
+++ laba1.2/laba1.2(1)/laba1.2(1).cpp
@@ -0,0 +1,89 @@
+﻿
+
+#include <iostream>
+#include <iomanip>
+
+const size_t n = 5;
+
+void freeArr1D(int* arr)
+{
+	delete[] arr;
+}
+
+void freeArr2D(int** arr)
+{
+	for (int i = 0; i < n; i++)
+	{
+		delete[] arr[i];
+	}
+}
+
+void print(int** arr2D, int rows, int cols)
+{
+	std::cout << std::endl;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			std::cout << std::setw(8) << *(*(arr2D + i) + j);
+		}
+		std::cout << std::endl;
+	}
+}
+
+int fact(int num)
+{
+	if (num == 0)
+		return 1;
+	else
+		return num * fact(num - 1);
+}
+
+void arr(int** arr2D, int rows, int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			*(*(arr2D + i) + j) = fact(i+j);
+		}
+	}
+	print(arr2D, n, n);
+}
+
+void NewArr(int** arr2D, int rows, int cols)
+{
+	int n = 10, rowsCnt = 1, cnt = 0;
+	int* newArr = new int[n];
+	std::cout << std::endl;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			if (rowsCnt % 2 == 0)
+			{
+				*(newArr + cnt) = *(*(arr2D + i) + j);
+				cnt++;
+			}
+		}
+		rowsCnt++;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << *(newArr + i) << " ";
+	}
+	freeArr1D(newArr);
+}
+
+void main()
+{
+	int** arr2D = new int* [n];
+	for (int i = 0; i < n; i++)
+	{
+		arr2D[i] = new int[n];
+	}
+	arr(arr2D, n, n);
+	NewArr(arr2D, n, n);
+	freeArr2D(arr2D);
+	system("pause");
+}
